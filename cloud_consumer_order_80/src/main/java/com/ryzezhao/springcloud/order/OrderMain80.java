8a75d25c0f92c758ab6876959380ac5e7eee0380
RyzeZhao <643080112@qq.com>
2020-05-07 17:15:35 +0800

    SpringCloud(H版)：将服务（cloud_provider_payment_8001、cloud_consumer_order_80）注册到cloud_eureka_server_7001

--- cloud_consumer_order_80/src/main/java/com/ryzezhao/springcloud/order/OrderMain80.java
+++ cloud_consumer_order_80/src/main/java/com/ryzezhao/springcloud/order/OrderMain80.java
@@ -2,9 +2,11 @@ package com.ryzezhao.springcloud.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
--- cloud_provider_payment_8001/src/main/java/com/ryzezhao/springcloud/payment/PaymentMain8001.java
+++ cloud_provider_payment_8001/src/main/java/com/ryzezhao/springcloud/payment/PaymentMain8001.java
@@ -2,8 +2,10 @@ package com.ryzezhao.springcloud.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class, args);
