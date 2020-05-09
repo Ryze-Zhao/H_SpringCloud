300088b547a5e74da731db5611515d76a8a5e63b
RyzeZhao <643080112@qq.com>
2020-05-09 17:24:09 +0800

    SpringCloud(H版)：Actuator微服务信息完善与服务发现Discovery（代码）； 将服务提供者（cloud_provider_payment_8001、cloud_provider_payment_8002） 与服务消费者（cloud_consumer_order_80） 注册到Eureka集群（cloud_eureka_server_7001、cloud_eureka_server_7002） 调用8001接口测试即可

--- cloud_provider_payment_8001/src/main/java/com/ryzezhao/springcloud/payment/controller/PaymentController.java
+++ cloud_provider_payment_8001/src/main/java/com/ryzezhao/springcloud/payment/controller/PaymentController.java
@@ -7,7 +7,12 @@ import com.ryzezhao.springcloud.payment.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -18,6 +23,9 @@ public class PaymentController {
 
     @Value("${server.port}")
     private String serverPort;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @PostMapping(value = "/payment")
     public Result saveOrUpdatePayment(@RequestBody Payment payment) {
@@ -39,23 +47,22 @@ public class PaymentController {
             return Result.error(HttpStatusConstant.SC_NO_RESPONSE_444, "没有对应记录,查询ID: " + id);
         }
     }
+
+    @GetMapping(value = "/payment/discovery")
+    public Object discovery() {
+        List<String> services = discoveryClient.getServices();
+        for (String element : services) {
+            log.info("已有服务: " + element);
+            List<ServiceInstance> instances = discoveryClient.getInstances(element);
+            for (ServiceInstance instance : instances) {
+                log.info(instance.getServiceId() + "\t" + instance.getHost() + "\t" + instance.getPort() + "\t" + instance.getUri());
+            }
+        }
+        return this.discoveryClient;
+    }
+
 }
 
-//
-//    @GetMapping(value = "/payment/discovery")
-//    public Object discovery() {
-//        List<String> services = discoveryClient.getServices();
-//        for (String element : services) {
-//            log.info("*****element: " + element);
-//        }
-//
-//        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
-//        for (ServiceInstance instance : instances) {
-//            log.info(instance.getServiceId() + "\t" + instance.getHost() + "\t" + instance.getPort() + "\t" + instance.getUri());
-//        }
-//
-//        return this.discoveryClient;
-//    }
 //
 //    @GetMapping(value = "/payment/lb")
 //    public String getPaymentLB() {
