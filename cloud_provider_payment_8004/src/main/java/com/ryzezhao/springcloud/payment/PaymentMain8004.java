ce1ab563ddc9169bd44199e59cb96f322b58da92
RyzeZhao <643080112@qq.com>
2020-05-13 17:54:09 +0800

    SpringCloud(H版)：将服务注册到Zookeeper（代码） 服务提供者（cloud_provider_payment_8004）

---
+++ cloud_provider_payment_8004/src/main/java/com/ryzezhao/springcloud/payment/PaymentMain8004.java
@@ -0,0 +1,11 @@
+package com.ryzezhao.springcloud.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentMain8004 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8004.class, args);
+    }
+}
