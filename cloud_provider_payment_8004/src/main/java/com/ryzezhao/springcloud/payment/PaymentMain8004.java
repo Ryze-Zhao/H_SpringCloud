4f01cbd1c89a45d779f7d0c01305d7bc439cbe9a
RyzeZhao <643080112@qq.com>
2020-05-14 11:02:30 +0800

    SpringCloud(H版)：将服务注册到Zookeeper（代码） 服务提供者（cloud_provider_payment_8004）

--- cloud_provider_payment_8004/src/main/java/com/ryzezhao/springcloud/payment/PaymentMain8004.java
+++ cloud_provider_payment_8004/src/main/java/com/ryzezhao/springcloud/payment/PaymentMain8004.java
@@ -2,8 +2,11 @@ package com.ryzezhao.springcloud.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+//该注解用于向使用consul或者zookeeper作为注册中心时注册服务
+@EnableDiscoveryClient
 public class PaymentMain8004 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8004.class, args);
--- cloud_provider_payment_8004/src/main/java/com/ryzezhao/springcloud/payment/controller/PaymentController.java
+++ cloud_provider_payment_8004/src/main/java/com/ryzezhao/springcloud/payment/controller/PaymentController.java
@@ -3,12 +3,14 @@ package com.ryzezhao.springcloud.payment.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
 @RestController
 @Slf4j
+@RequestMapping("/cloud/pay")
 public class PaymentController
 {
     @Value("${server.port}")
@@ -17,6 +19,6 @@ public class PaymentController
     @GetMapping(value = "/payment/zk")
     public String paymentzk()
     {
-        return "springcloud with zookeeper: "+serverPort+"\t"+ UUID.randomUUID().toString();
+        return "Springcloud With Zookeeper: "+serverPort+"\t"+ UUID.randomUUID().toString();
     }
 }
