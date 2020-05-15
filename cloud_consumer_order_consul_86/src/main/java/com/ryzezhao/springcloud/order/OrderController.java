561fd08253cd13eb8dab070d58e46402d5150952
RyzeZhao <643080112@qq.com>
2020-05-15 10:56:43 +0800

    SpringCloud(H版)：将服务消费者86注册到Consul（代码） 涉及模块： 服务提供者（cloud_provider_payment_8006） 服务消费者（cloud_consumer_order_consul_86）

---
+++ cloud_consumer_order_consul_86/src/main/java/com/ryzezhao/springcloud/order/OrderController.java
@@ -0,0 +1,27 @@
+package com.ryzezhao.springcloud.order;
+
+
+import com.ryzezhao.springcloud.order.common.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+
+@RestController
+@Slf4j
+@RequestMapping("/cloud/consumer")
+public class OrderController {
+    /** 服务提供者的URL*/
+//    public static final String PAYMENT_URL = "http://localhost:8001";
+    /** 服务提供者的spring.application.name，可从服务提供者的yml找到名称*/
+    public static final String PAYMENT_URL = "http://cloud-payment-service";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/payment/consul")
+    public Result getPayment() {
+        return restTemplate.getForObject(PAYMENT_URL + "/cloud/pay/payment/consul", Result.class);
+    }
+}
