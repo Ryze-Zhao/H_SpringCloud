18885f462799519d5332e10f6dd8681a9fd7b4b1
RyzeZhao <643080112@qq.com>
2020-05-19 17:16:10 +0800

    SpringCloud(H版)：在服务消费者80替换Ribbon负载规则（代码） 服务提供者集群（cloud_provider_payment_8001、cloud_provider_payment_8002） 服务消费者（cloud_consumer_order_80） Eureka集群（cloud_eureka_server_7001、cloud_eureka_server_7002）

--- cloud_consumer_order_80/src/main/java/com/ryzezhao/springcloud/order/OrderMain80.java
+++ cloud_consumer_order_80/src/main/java/com/ryzezhao/springcloud/order/OrderMain80.java
@@ -1,12 +1,15 @@
 package com.ryzezhao.springcloud.order;
 
+import com.ryzezhao.springcloud.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "cloud-payment-service",configuration= MySelfRule.class)
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
