95a5cfc6a4f10ea0ef6653bb3e70734ed024cd22
RyzeZhao <643080112@qq.com>
2020-05-25 17:36:06 +0800

    SpringCloud(H版)：Hystrix服务降级（代码）:服务提供者端侧降级 服务消费者（cloud_consumer_order_feign_hystrix_80） 服务提供者（cloud_provider_payment_8001） Eureka单机（cloud_eureka_server_7001） 公共模块（cloud_api_commons）

--- cloud_provider_payment_hystrix_8001/src/main/java/com/ryzezhao/springcloud/payment/PaymentHystrixMain8001.java
+++ cloud_provider_payment_hystrix_8001/src/main/java/com/ryzezhao/springcloud/payment/PaymentHystrixMain8001.java
@@ -2,10 +2,12 @@ package com.ryzezhao.springcloud.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
 public class PaymentHystrixMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentHystrixMain8001.class, args);
--- cloud_provider_payment_hystrix_8001/src/main/java/com/ryzezhao/springcloud/payment/service/impl/PaymentServiceImpl.java
+++ cloud_provider_payment_hystrix_8001/src/main/java/com/ryzezhao/springcloud/payment/service/impl/PaymentServiceImpl.java
@@ -18,14 +18,22 @@ public class PaymentServiceImpl extends ServiceImpl<PaymentMapper, Payment> impl
         return "线程池:  " + Thread.currentThread().getName() + "  paymentInfo_OK,id:  " + id + "  O(∩_∩)O哈哈~";
     }
 
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOut_Fallback", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "6000")
+    })
     @Override
     public String paymentInfo_TimeOut(Integer id) {
-        //int age = 10/0;
+        //我们也可以人为制造异常，也是会调用fallbackMethod
+//        int age = 10/0;
         try {
             TimeUnit.MILLISECONDS.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        return "线程池:  " + Thread.currentThread().getName() + "  paymentInfo_TimeOut id:  " + id + "   O(∩_∩)O哈哈~" ;
+        return "线程池:  " + Thread.currentThread().getName() + "  paymentInfo_TimeOut id:  " + id + "   O(∩_∩)O哈哈~";
+    }
+
+    public String paymentInfo_TimeOut_Fallback(Integer id) {
+        return "线程池:  " + Thread.currentThread().getName() + "  8001系统繁忙或者运行报错，请稍后再试,id:  " + id + "    o(╥﹏╥)o呜呜";
     }
 }
