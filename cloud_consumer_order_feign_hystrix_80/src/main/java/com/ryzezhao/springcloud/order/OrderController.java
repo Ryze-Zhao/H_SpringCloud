38ae85857580b4cea87080987b4b7964b5bfdb0d
RyzeZhao <643080112@qq.com>
2020-05-25 20:39:05 +0800

    SpringCloud(H版)：Hystrix服务降级（代码）:全局服务降级@DefaultProperties； 服务消费者（cloud_consumer_order_feign_hystrix_80） 服务提供者（cloud_provider_payment_8001） Eureka单机（cloud_eureka_server_7001） 公共模块（cloud_api_commons）

--- cloud_consumer_order_feign_hystrix_80/src/main/java/com/ryzezhao/springcloud/order/OrderController.java
+++ cloud_consumer_order_feign_hystrix_80/src/main/java/com/ryzezhao/springcloud/order/OrderController.java
@@ -1,6 +1,7 @@
 package com.ryzezhao.springcloud.order;
 
 
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.ryzezhao.springcloud.common.Result;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
+@DefaultProperties(defaultFallback = "payment_Global_FallBack")
 @RequestMapping("/cloud/consumer")
 public class OrderController {
     @Autowired
@@ -27,13 +29,14 @@ public class OrderController {
         return paymentVOResult;
     }
 
-    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOut_Fallback", commandProperties = {
+/*    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOut_Fallback", commandProperties = {
             @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
-    })
+    })*/
+    @HystrixCommand
     @GetMapping("/hystrix/timeout/{id}")
     public Result paymentInfo_TimeOut(@PathVariable("id") Integer id) {
         //我们也可以人为制造异常，也是会调用fallbackMethod
-//        int age = 10/0;
+        int age = 10/0;
         Result<String> paymentVOResult = paymentFeignHystrixService.paymentInfo_TimeOut(id);
         log.info("*****result: " + paymentVOResult.getResult());
         return paymentVOResult;
@@ -42,5 +45,9 @@ public class OrderController {
     public Result paymentInfo_TimeOut_Fallback(Integer id) {
         String result = "服务消费者80：服务提供者8001系统繁忙或者本服务运行出错请检查,o(╥﹏╥)o呜呜" + id;
         return Result.error(result);
+    }
+
+    public Result payment_Global_FallBack() {
+        return Result.error("Global异常处理信息，请稍后再试，/(ㄒoㄒ)/~~");
     }
 }
