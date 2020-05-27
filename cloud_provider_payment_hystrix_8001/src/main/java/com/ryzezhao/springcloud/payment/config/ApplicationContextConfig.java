473f75d2d3876f40544517c30788ddf7d241b6c7
RyzeZhao <643080112@qq.com>
2020-05-27 16:19:29 +0800

    SpringCloud(H版)：	SpringCloud(H版)：Hystrix图形化Dashboard（介绍与代码）
    HystrixDashboard（cloud_hystrix_dashboard_18001）
    服务提供者（cloud_provider_payment_8001）
    Eureka单机（cloud_eureka_server_7001）
    公共模块（cloud_api_commons）

---
+++ cloud_provider_payment_hystrix_8001/src/main/java/com/ryzezhao/springcloud/payment/config/ApplicationContextConfig.java
@@ -0,0 +1,25 @@
+package com.ryzezhao.springcloud.payment.config;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+
+@Configuration
+public class ApplicationContextConfig {
+    /**
+     *此配置是为了服务监控而配置，与服务容错本身无关，springcloud升级后的坑,ServletRegistrationBean因为springboot的默认路径不是"/hystrix.stream"，
+     *只要在自己的项目里配置上下面的servlet就可以了
+     */
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+}
