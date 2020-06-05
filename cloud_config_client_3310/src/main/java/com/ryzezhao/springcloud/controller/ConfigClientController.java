468b8cdf59e8f5ccf94edecbd81f1e7d6e24802e
RyzeZhao <643080112@qq.com>
2020-06-05 17:26:07 +0800

    SpringCloud(H版)：Config配置消费者（代码）；手动刷新配置；
    Eureka单机：cloud_eureka_server_7001
    ConfigServer：cloud_config_server_3300
    ConfigClient：cloud_config_client_3310
    公共模块：cloud_api_commons

--- cloud_config_client_3310/src/main/java/com/ryzezhao/springcloud/controller/ConfigClientController.java
+++ cloud_config_client_3310/src/main/java/com/ryzezhao/springcloud/controller/ConfigClientController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ConfigClientController {
     @Value("${config.info}")
     private String configInfo;
