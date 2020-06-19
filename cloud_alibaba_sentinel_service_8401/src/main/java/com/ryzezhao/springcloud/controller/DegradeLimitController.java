cd25aed982831b8cdfe6ca1ee0a3706599b217d6
RyzeZhao <643080112@qq.com>
2020-06-19 10:51:19 +0800

    SpringCloud(H版)：Sentinel降级介绍及演示；演示：Sentinel降级之RT
    注册中心：Nacos
    轻量级高可用流量控制：Sentinel
    演示模块：cloud_alibaba_sentinel_service_8401

---
+++ cloud_alibaba_sentinel_service_8401/src/main/java/com/ryzezhao/springcloud/controller/DegradeLimitController.java
@@ -0,0 +1,24 @@
+package com.ryzezhao.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 降级演示
+ *
+ * @author: HeHaoZhao
+ * @date: 2020/6/17 19:29
+ */
+@RestController
+@Slf4j
+public class DegradeLimitController {
+    @GetMapping("/testF")
+    public String testF() {
+        try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
+        log.info("testF 测试RT");
+        return "------testF";
+    }
+}
